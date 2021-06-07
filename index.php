8c966d0620472cc0940cb8f9b6d48f69c8f7261e
ste7el <batista07danilo@gmail.com>
2021-06-07 20:25:08 -0300

    Tipos de variaveis

---
+++ index.php
@@ -0,0 +1,23 @@
+<h2> Tipos de dados e Variaveis </h2>
+<?php
+
+
+
+$idade = 25; //Tipo de dado número inteiro
+$status = true; //Tipo de dado boliano verdadeiro ou falso
+$valor = 1.50; //Tipo de valor flutuante 
+$palavras = "string"; //Tipo de dado para palavras
+
+echo "minha idade é $idade ";
+echo " <br>";
+
+$idade = 30;
+
+echo ' minha idade é '. $idade;
+
+
+
+
+
+
+?>
